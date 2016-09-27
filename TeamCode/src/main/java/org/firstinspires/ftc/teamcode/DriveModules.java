4eda307abb7f9e89e2388fdc0ea12d4a6229125f
SourMongoose <chrisxiongyang@yahoo.com>
2016-09-26 17:25:37 -0700

    Optimized HolonomicDrive
    Shortened code, renamed variables properly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveModules.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveModules.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import java.lang.Math;
 
 /**
  * This is NOT an opmode.
@@ -16,30 +17,27 @@ import com.qualcomm.robotcore.util.Range;
  * <p>
  * <p>
  * FOR HOLONOMIC:
- * FRONT LEFT  MOTOR:   "FrontLeft"
- * FRONT RIGHT MOTOR:   "FrontRight"
- * BACK  LEFT  MOTOR:   "BackLeft"
- * BACK  RIGHT MOTOR:   "BackRight"
+ * FRONT LEFT  MOTOR:   "frontLeft"
+ * FRONT RIGHT MOTOR:   "frontRight"
+ * BACK  LEFT  MOTOR:   "backLeft"
+ * BACK  RIGHT MOTOR:   "backRight"
  * <p>
  * FOR STANDARD 2-MOTOR DRIVE:
- * Left  Motor: "Left"
- * Right Motor: "Right"
+ * Left  Motor: "left"
+ * Right Motor: "right"
  */
 public class DriveModules {
-    // Declaration of motors for Holonomic Drive
-    public DcMotor FrontLeft = null;
-    public DcMotor FrontRight = null;
-    public DcMotor BackLeft = null;
-    public DcMotor BackRight = null;
-    public DcMotor FlyWheel = null;
-    // Declaration of motors for Standard Drive
-    public DcMotor Left = null;
-    public DcMotor Right = null;
+    //Declaration of motors for Holonomic Drive
+    public DcMotor frontLeft = null,
+        frontRight = null,
+        backLeft = null,
+        backRight = null,
+        flyWheel = null,
+    //Declaration of motors for Standard Drive
+        left = null,
+        right = null;
     //Declare and Initialise Holonomic variables
-    Double FLPwr;
-    Double FRPwr;
-    Double BLPwr;
-    Double BRPwr;
+    private double flPwr, frPwr, blPwr, brPwr;
     /* local OpMode members. */
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
@@ -54,120 +52,85 @@ public class DriveModules {
         // Save reference to Hardware map
         hwMap = ahwMap;
         // Initialize Flywheel Motor
-        FlyWheel = hwMap.dcMotor.get("FlyWheel");
+        flyWheel = hwMap.dcMotor.get("FlyWheel");
 
 
         // Initialize Holonomic Motors
-        FrontLeft = hwMap.dcMotor.get("FrontLeft");
-        FrontRight = hwMap.dcMotor.get("FrontRight");
-        BackLeft = hwMap.dcMotor.get("BackLeft");
-        BackRight = hwMap.dcMotor.get("BackRight");
+        frontLeft = hwMap.dcMotor.get("frontLeft");
+        frontRight = hwMap.dcMotor.get("frontRight");
+        backLeft = hwMap.dcMotor.get("backLeft");
+        backRight = hwMap.dcMotor.get("backRight");
 
         //Initialize Standard Drive Motors
-        Left = hwMap.dcMotor.get("Left");
-        Right = hwMap.dcMotor.get("Right");
+        left = hwMap.dcMotor.get("Left");
+        right = hwMap.dcMotor.get("Right");
 
         //set wheel direction (reverse if on the right)
-        FrontLeft.setDirection(DcMotor.Direction.FORWARD);
-        FrontRight.setDirection(DcMotor.Direction.REVERSE);
-        BackLeft.setDirection(DcMotor.Direction.FORWARD);
-        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
 
-        Left.setDirection(DcMotor.Direction.FORWARD);
-        Right.setDirection(DcMotor.Direction.REVERSE);
+        left.setDirection(DcMotor.Direction.FORWARD);
+        right.setDirection(DcMotor.Direction.REVERSE);
 
         // Set all motors to zero power
-        FlyWheel.setPower(0);
+        flyWheel.setPower(0);
 
-        FrontLeft.setPower(0);
-        FrontRight.setPower(0);
-        BackLeft.setPower(0);
-        BackRight.setPower(0);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
 
-        Left.setPower(0);
-        Right.setPower(0);
-
-        // Set all motors to run without encoders.
-        // May want to use RUN_USING_ENCODERS if encoders are installed.
-
-
+        left.setPower(0);
+        right.setPower(0);
     }
 
     //Tank Drive Method
-    public void Tank(Double LeftPwr, Double RightPwr) {
-
+    public void Tank(double leftPwr, double rightPwr) {
         //Balance inputs greater than 1 for both motors.
-        //Note: does not account for when (LeftPwr + RightPwr) is greater 2 or less than -2
-        if (LeftPwr > 1) {
-            RightPwr -= (LeftPwr - 1);
-            LeftPwr = 1.0;
+        //Note: does not account for when (leftPwr + rightPwr) is greater 2 or less than -2
+        if (leftPwr > 1) {
+            rightPwr -= (leftPwr - 1);
+            leftPwr = 1;
         }
-        if (LeftPwr < -1) {
-            RightPwr += (LeftPwr + 1);
-            LeftPwr = -1.0;
+        if (leftPwr < -1) {
+            rightPwr += (leftPwr + 1);
+            leftPwr = -1;
         }
-        if (RightPwr > 1) {
-            LeftPwr -= (RightPwr - 1);
-            RightPwr = 1.0;
+        if (rightPwr > 1) {
+            leftPwr -= (rightPwr - 1);
+            rightPwr = 1;
         }
-        if (RightPwr < -1) {
-            LeftPwr -= (RightPwr + 1);
-            RightPwr = -1.0;
+        if (rightPwr < -1) {
+            leftPwr -= (rightPwr + 1);
+            rightPwr = -1;
         }
 
 
-        Left.setPower(LeftPwr);
-        Right.setPower(RightPwr);
-
+        left.setPower(leftPwr);
+        right.setPower(rightPwr);
     }
 
     //Holonomic Drive Method
-    public void Holonomic(double movX, Double movY, Double rot) {
+    public void Holonomic(double movX, double movY, double rot) {
 
         //assign motor powers their inputs based on position on the robot
-        FLPwr = Range.clip((+movX + movY), -1, 1) + rot;
-        FRPwr = Range.clip((-movX + movY), -1, 1) - rot;
-        BLPwr = Range.clip((-movX + movY), -1, 1) + rot;
-        BRPwr = Range.clip((+movX + movY), -1, 1) - rot;
+        flPwr = Range.clip((+movX + movY), -1, 1) + rot;
+        frPwr = Range.clip((-movX + movY), -1, 1) - rot;
+        blPwr = Range.clip((-movX + movY), -1, 1) + rot;
+        brPwr = Range.clip((+movX + movY), -1, 1) - rot;
 
         //balance movement and rotation
-        if (FLPwr > 1) {
-            BRPwr -= (FLPwr - 1);
-            FLPwr = 1.0;
-        }
-        if (FLPwr < -1) {
-            BRPwr += (FLPwr + 1);
-            FLPwr = -1.0;
-        }
-        if (FRPwr > 1) {
-            BLPwr -= (FRPwr - 1);
-            FRPwr = 1.0;
-        }
-        if (FRPwr < -1) {
-            BLPwr += (FRPwr + 1);
-            FRPwr = -1.0;
-        }
-        if (BLPwr > 1) {
-            FRPwr -= (BLPwr - 1);
-            BLPwr = 1.0;
-        }
-        if (BLPwr < -1) {
-            FRPwr += (BLPwr + 1);
-            BLPwr = -1.0;
-        }
-        if (BRPwr > 1) {
-            FLPwr -= (BRPwr - 1);
-            BRPwr = 1.0;
-        }
-        if (BRPwr < -1) {
-            FLPwr += (BRPwr + 1);
-            BRPwr = -1.0;
-        }
+        brPwr -= Math.max(flPwr-1, 0) - Math.min(flPwr+1, 0);
+        blPwr -= Math.max(frPwr-1, 0) - Math.min(frPwr+1, 0);
+        frPwr -= Math.max(blPwr-1, 0) - Math.min(blPwr+1, 0);
+        flPwr -= Math.max(brPwr-1, 0) - Math.min(brPwr+1, 0);
 
-        FrontLeft.setPower(FLPwr);
-        FrontRight.setPower(FRPwr);
-        BackLeft.setPower(BLPwr);
-        BackRight.setPower(BRPwr);
+        frontLeft.setPower(flPwr);
+        frontRight.setPower(frPwr);
+        backLeft.setPower(blPwr);
+        backRight.setPower(brPwr);
     }
 
     /***
