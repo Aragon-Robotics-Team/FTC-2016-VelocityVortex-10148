fffa5c20ac6626c8fecfea5a6176f1d17d41ffbb
SourMongoose <chrisxiongyang@yahoo.com>
2016-10-03 16:50:51 -0700

    Converted XDrive from LinearOpMode to OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XDrive.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-public class XDrive extends LinearOpMode {
+@TeleOp(name = "XDrive", group = "Holonomic")
+public class XDrive extends OpMode {
     DcMotor motorBackLeft;
     DcMotor motorBackRight;
     DcMotor motorFrontLeft;
@@ -16,7 +18,7 @@ public class XDrive extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
 
         motorBackLeft = hardwareMap.dcMotor.get("bl");
         motorBackRight = hardwareMap.dcMotor.get("br");
@@ -27,47 +29,57 @@ public class XDrive extends LinearOpMode {
         motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
 
-        waitForStart();
         runtime.reset();
+    }
 
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
+    @Override
+    public void init_loop() {
+    }
 
-            // the left stick drives sideways (left and right), forward (up), and backwards(down)
-            // the right stick does all the turns
-            // only one gamepad
-            // y=x^5
+    @Override
+    public void start() {
+    }
 
-            if (gamepad2.right_bumper) {
-                motorFrontRight.setPower(Math.pow(Range.clip(gamepad2.right_stick_x - gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
-                motorBackRight.setPower(Math.pow(Range.clip(gamepad2.right_stick_x + gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
-                motorBackLeft.setPower(Math.pow(Range.clip(-gamepad2.right_stick_x + gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
-                motorFrontLeft.setPower(Math.pow(Range.clip(-gamepad2.right_stick_x - gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.update();
 
-                if (gamepad2.dpad_up) {
-                    spin.setPower(-0.6);
-                } else if (gamepad2.dpad_down) {
-                    spin.setPower(0.2);
-                } else {
-                    spin.setPower(-0.1);
-                }
+        // the left stick drives sideways (left and right), forward (up), and backwards(down)
+        // the right stick does all the turns
+        // only one gamepad
+        // y=x^5
+
+        if (gamepad2.right_bumper) {
+            motorFrontRight.setPower(Math.pow(Range.clip(gamepad2.right_stick_x - gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
+            motorBackRight.setPower(Math.pow(Range.clip(gamepad2.right_stick_x + gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
+            motorBackLeft.setPower(Math.pow(Range.clip(-gamepad2.right_stick_x + gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
+            motorFrontLeft.setPower(Math.pow(Range.clip(-gamepad2.right_stick_x - gamepad2.left_stick_x + gamepad2.left_stick_y, -1, 1), 5));
+
+            if (gamepad2.dpad_up) {
+                spin.setPower(-0.6);
+            } else if (gamepad2.dpad_down) {
+                spin.setPower(0.2);
             } else {
-                motorFrontRight.setPower(Math.pow(Range.clip(gamepad1.right_stick_x - gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
-                motorBackRight.setPower(Math.pow(Range.clip(gamepad1.right_stick_x + gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
-                motorBackLeft.setPower(Math.pow(Range.clip(-gamepad1.right_stick_x + gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
-                motorFrontLeft.setPower(Math.pow(Range.clip(-gamepad1.right_stick_x - gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
-
-                if (gamepad1.dpad_up) {
-                    spin.setPower(-0.6);
-                } else if (gamepad1.dpad_down) {
-                    spin.setPower(0.2);
-                } else {
-                    spin.setPower(-0.1);
-                }
+                spin.setPower(-0.1);
             }
+        } else {
+            motorFrontRight.setPower(Math.pow(Range.clip(gamepad1.right_stick_x - gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
+            motorBackRight.setPower(Math.pow(Range.clip(gamepad1.right_stick_x + gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
+            motorBackLeft.setPower(Math.pow(Range.clip(-gamepad1.right_stick_x + gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
+            motorFrontLeft.setPower(Math.pow(Range.clip(-gamepad1.right_stick_x - gamepad1.left_stick_x + gamepad1.left_stick_y, -1, 1), 5));
 
-            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+            if (gamepad1.dpad_up) {
+                spin.setPower(-0.6);
+            } else if (gamepad1.dpad_down) {
+                spin.setPower(0.2);
+            } else {
+                spin.setPower(-0.1);
+            }
         }
+    }
+
+    @Override
+    public void stop() {
     }
 }
