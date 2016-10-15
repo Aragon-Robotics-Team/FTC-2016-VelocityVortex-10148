0d0382b54fac1dfb2e85371ac14982c0ea68e732
Akshay Bodla <ab11dec00@gmail.com>
2016-10-15 15:16:49 -0700

    Wrote a simple flywheel class
     This is only a template.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSingle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSingle.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.subsystems.*;
 
@@ -8,22 +9,33 @@ import org.firstinspires.ftc.teamcode.subsystems.*;
 public class TeleOpSingle extends OpMode {
 
     private Drivetrain drivetrain;
+    private FlyWheel flyWheel;
 
     public void init() {
         drivetrain = new Drivetrain(hardwareMap.dcMotor.get("drive_front_left"), hardwareMap.dcMotor.get("drive_front_right"), hardwareMap.dcMotor.get("drive_back_left"), hardwareMap.dcMotor.get("drive_back_right"));
         drivetrain.stop();
+        // flyWheel = new FlyWheel(hardwareMap.dcMotor.get("fly_wheel"));
     }
 
     @Override
     public void start() {
         drivetrain.stop();
+        // flyWheel.stop();
     }
 
     @Override
     public void loop() {
         drivetrain.holonomicDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+       /*
+       if (gamepad1.a == true)
+            flyWheel.setMaxPow();
+        else
+            flyWheel.stop();
+       */
     }
     public void stop() {
         drivetrain.stop();
+        flyWheel.stop();
     }
 }
