e2bbfe859bbd3ea49bf46b6b1f85578f2c9c237b
Albert Liu <19liua28@myahs.org>
2016-11-04 17:31:00 -0700

    created private branch for drivesystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSingle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpSingle.java
@@ -11,35 +11,23 @@ import org.firstinspires.ftc.teamcode.subsystems.*;
 public class TeleOpSingle extends OpMode {
 
     private Drivetrain drivetrain;
-    private FlyWheel flyWheel;
-    private ConveyorBelt conveyor;
 
     public void init() {
         drivetrain = new Drivetrain(hardwareMap.dcMotor.get("drive_front_left"), hardwareMap.dcMotor.get("drive_front_right"), hardwareMap.dcMotor.get("drive_back_left"), hardwareMap.dcMotor.get("drive_back_right"));
         drivetrain.stop();
-
-        flyWheel = new FlyWheel(hardwareMap.dcMotor.get("fly_wheel_forward"), hardwareMap.dcMotor.get("other_fly_wheel_"), hardwareMap.servo.get("gate_servo"));
-
     }
 
     @Override
     public void start() {
         drivetrain.stop();
-        flyWheel.start();
-        conveyor.start();
     }
 
     @Override
     public void loop() {
         drivetrain.holonomicDrive(Math.pow(gamepad1.left_stick_x,7), Math.pow(gamepad1.left_stick_y,7), Math.pow(gamepad1.right_stick_x,7));
-
-        flyWheel.setPower(gamepad1.right_trigger); // assigns motor power
-
     }
 
     public void stop() {
         drivetrain.stop();
-        flyWheel.start();
-        conveyor.start();
     }
 }
